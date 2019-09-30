915e8de289cd91b7f18863998d198bf161925813
Sayed Ahmad Sahim <sayedahmad.sahim@gmail.com>
2019-09-30 11:57:13 +0430

    bot setup

---
+++ tracksCustomers/chatbot/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('get_response/', views.get_response, name="get_response"),
+      
+]
--- tracksCustomers/chatbot/views.py
+++ tracksCustomers/chatbot/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from .chatbot import *
+
+
+# Train based on the english corpus
+
+#Already trained and it's supposed to be persistent
+# chatbot.train("chatterbot.corpus.english")
+
+@csrf_exempt
+def get_response(request):
+    response = {'status': None}
+    if request.method == 'POST':
+        message=request.POST['message']
+        chat_response = chatbot.get_response(message).text
+		# response['message'] = {'text': chat_response, 'user': False, 'chat_bot': True}
+		# response['status'] = 'ok'
+        return HttpResponse(chat_response)
+    # else:
+    #     return HttpResponse('no response')
+
+
+
+def home(request):
+	return render(request,"chatbot/chat.html")
